cfb5e7361a1d9b7ad51263313592bee698016ecf
Explv <Explv@osbot.org>
2018-02-23 14:12:41 +0000

    Updating to OSBot 2.5.2

--- osbot_manager/src/main/Version.java
+++ osbot_manager/src/main/Version.java
@@ -8,7 +8,7 @@ import java.net.URL;
 
 public class Version {
 
-    private final static float currentVersion = 7.9f;
+    private final static float currentVersion = 7.10f;
 
     public static boolean isLatestVersion() {
         try {
